cc6deef3c23b179385606e4495be255ec8811c18
ass52 <96520024+ass52@users.noreply.github.com>
2022-01-19 18:34:44 +0530

    master

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,6 +12,7 @@
                         @csrf
 
                         <div class="row mb-3">
+                            
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
